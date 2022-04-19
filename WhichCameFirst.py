b106a53f86c3d5ec736cdca99f51137bae64a61e
Jamie H <jamie.horsfield2018@gmail.com>
2022-04-19 17:19:08 +0100

    -Added pick_items method and basic functionality for play_game method

--- WhichCameFirst.py
+++ WhichCameFirst.py
@@ -1,9 +1,14 @@
 #Class that models the game itself. Uses the Item class for its content.
 
 import Item
+import random
 class WhichCameFirst:
     def __init__(self):
         self.running = True
+        self.playing = True
+        self.game_start_message = "The game will present you with option A and B.\nChoose whichever you think is older"\
+            " to get a point."
+        self.score = 0
         #some default examples for testing
         mark_twain = Item.Item("Mark Twain", 1835)
         bicycle = Item.Item("The bicycle", 1817)
@@ -34,8 +39,59 @@ class WhichCameFirst:
         user_input = input()
         return user_input
 
+    def pick_items(self):
+        """Method called by play game function to select two random items to play the game with"""
+        positions = []
+        for i in range(0, 2):
+            positions.append(random.randint(0, len(self.item_list)-1))
+        chosen_items = []
+        chosen_items.append(self.item_list[positions[0]])
+        chosen_items.append(self.item_list[positions[1]])
+        return(chosen_items)
+        print("A) " + chosen_items[0])
+        print("Or...")
+        print("B) " + chosen_items[1])
+        
+
+
     def play_game(self):
+        """Method called when user chooses run game option."""
+        print(self.game_start_message)
+        score = 0
+        while self.playing:
+            print("You currently have " + str(self.score) + " points.")
+            print("Enter A or B to make a choice or X to quit the game")
+            print("Which came first?: ")
+            options = self.pick_items()
+            if options[0].age > options[1].age:
+                correct = "A"
+            else:
+                correct = "B"
+            print("A) " + options[0].get_name())
+            print("Or...")
+            print("B) " + options[1].get_name())
+            print("(Press X to quit.)")
+            response = self.get_input()
+            print("You chose: " + response)
+            if response.upper() == correct:
+                score = score + 1
+                print("Correct!")
+            elif response.upper() == "X":
+                print("Thanks for playing.")
+                break
+            else:
+                print("Incorrect!")
         pass
+
+        """
+            print rules and instructions
+            while running:
+                show question
+                    if correct add point
+                    if wrong pass
+                    if end game, end game
+                show new question
+            """
 
     #Use this method just to show item names
     #functional
