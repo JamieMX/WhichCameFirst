d3beddc16e0d9cd16522dd03267a185c4b167f7f
Jamie H <jamie.horsfield2018@gmail.com>
2022-04-20 15:24:30 +0100

    Testing file

---
+++ Testing.py
@@ -0,0 +1,10 @@
+import WhichCameFirst
+
+#file used for testing purposes
+
+def main():
+    test1 =  WhichCameFirst.WhichCameFirst()
+    test1.start_program()
+
+if __name__ == "__main__":
+    main()
